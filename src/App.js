1460992e6dbbf5d14622a82ae11f453aace8805a
Abdulkadir Musse <abdul@Abdulkadirs-MacBook-Pro.local>
2021-11-16 21:19:05 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
 
             setData(dataArr);
             setLoading(false);
+            setError(false);
         })();
     }, []);
 
--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { useState } from "react";
 
-// Create a search function that you can search for a book
-function Search() {
+
 
 // Create a handle search function 
     function handleSearch(e){
-
+        e.preventDefault();
     }
 
     return (
@@ -14,6 +12,6 @@ function Search() {
             <input id="search" type="text" placeholder="ENTER BOOK NAME HERE" className="input-holder"onChange={handleSearch}></input>
         </div>
     )
-}
+
 
 export default Search;
