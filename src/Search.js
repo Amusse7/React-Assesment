adb11ab717c721a3e5b2316118e7f253712c9e4a
Abdulkadir Musse <abdul@Abdulkadirs-MacBook-Pro.local>
2021-11-16 21:26:53 -0500

    deploy bugs

--- src/Search.js
+++ src/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
-
+// Create a search function that you can search for a book
+function Search() {
 
 // Create a handle search function 
     function handleSearch(e){
         e.preventDefault();
+
     }
 
     return (
@@ -12,6 +15,6 @@ import React from "react";
             <input id="search" type="text" placeholder="ENTER BOOK NAME HERE" className="input-holder"onChange={handleSearch}></input>
         </div>
     )
-
+}
 
 export default Search;
